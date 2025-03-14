4f5d55b439f5e8d716feea8d8a92698eb9c35035
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-03-14 13:06:07 +0100

    minimal repro on radv

--- examples/swapchain_repro/swapchain_repro.cpp
+++ examples/swapchain_repro/swapchain_repro.cpp
@@ -1,50 +1,50 @@
 #include "imr/imr.h"
-#include "imr/util.h"
 
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     auto window = glfwCreateWindow(1024, 1024, "Example", nullptr, nullptr);
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
 
     imr::Context context;
 
     VkSurfaceKHR surface;
     glfwCreateWindowSurface(context.instance, window, nullptr, &surface);
 
-    VkSemaphore acquired;
-    vkCreateSemaphore(context.device, tmp((VkSemaphoreCreateInfo) {
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-    }), nullptr, &acquired);
-
-    uint32_t image_index;
     auto swapchain = vkb::SwapchainBuilder(context.physical_device, context.device, surface).build().value();
-    vkAcquireNextImageKHR(context.device, swapchain, 0, acquired, VK_NULL_HANDLE, &image_index);
 
-    VkCommandBuffer cmdbuf;
-    vkAllocateCommandBuffers(context.device, tmp((VkCommandBufferAllocateInfo) {
-        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
-        .commandPool = context.pool,
-        .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
-        .commandBufferCount = 1,
-    }), &cmdbuf);
+    VkSemaphore prev[32] = { };
 
-    vkBeginCommandBuffer(cmdbuf, tmp((VkCommandBufferBeginInfo) {
-        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-        .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
-    }));
-    vkEndCommandBuffer(cmdbuf);
-    vkQueueSubmit(context.main_queue, 1, tmp((VkSubmitInfo) {
-        .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
-        .waitSemaphoreCount = 1,
-        .pWaitSemaphores = &acquired,
-        .pWaitDstStageMask = tmp((VkPipelineStageFlags) VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT),
-        .commandBufferCount = 1,
-        .pCommandBuffers = &cmdbuf,
-        .signalSemaphoreCount = 0,
-    }), VK_NULL_HANDLE);
+    for (;;) {
+        VkSemaphore acquired;
+        vkCreateSemaphore(context.device, tmp((VkSemaphoreCreateInfo) {
+            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+        }), nullptr, &acquired);
+
+        VkFence fence;
+        vkCreateFence(context.device, tmp((VkFenceCreateInfo) {
+            .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+        }), nullptr, &fence);
+
+        uint32_t image_index;
+        vkAcquireNextImageKHR(context.device, swapchain, 0, acquired, fence, &image_index);
+        // make sure we 100% are done
+        vkWaitForFences(context.device, 1, &fence, true, UINT64_MAX);
+        vkDestroyFence(context.device, fence, nullptr);
+
+        if (prev[image_index])
+            vkDestroySemaphore(context.device, prev[image_index], nullptr);
+        prev[image_index] = acquired;
+
+        vkQueuePresentKHR(context.main_queue, tmp((VkPresentInfoKHR) {
+            .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+            .waitSemaphoreCount = 1,
+            .pWaitSemaphores = &acquired,
+            .swapchainCount = 1,
+            .pSwapchains = &swapchain.swapchain,
+            .pImageIndices = &image_index,
+        }));
+    }
 
     return 0;
 }
