4bf415d18d45237c5cf1230927bbb6daff461868
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-04-01 17:38:40 +0200

    10: resize support!

--- examples/10_intro/10_intro.cpp
+++ examples/10_intro/10_intro.cpp
@@ -2,22 +2,21 @@
 
 int main() {
     glfwInit();
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     auto window = glfwCreateWindow(1024, 1024, "Example", nullptr, nullptr);
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
 
     imr::Context context;
-    imr::Swapchain swapchain(context, window);
+    std::unique_ptr<imr::Swapchain> swapchain = std::make_unique<imr::Swapchain>(context, window);
     imr::FpsCounter fps_counter;
 
     // CPU-side staging buffer
     uint8_t* framebuffer = reinterpret_cast<uint8_t*>(malloc(width * height * 4));
 
-    imr::Buffer buffer(context, width * height * 4, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+    std::unique_ptr<imr::Buffer> buffer = std::make_unique<imr::Buffer>(context, width * height * 4, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
     uint8_t* mapped_buffer;
-    CHECK_VK(vkMapMemory(context.device, buffer.memory, buffer.memory_offset, buffer.size, 0, (void**) &mapped_buffer), abort());
+    CHECK_VK(vkMapMemory(context.device, buffer->memory, buffer->memory_offset, buffer->size, 0, (void**) &mapped_buffer), abort());
 
     VkFence fence;
     vkCreateFence(context.device, tmp((VkFenceCreateInfo) {
@@ -26,6 +25,22 @@ int main() {
     }), nullptr, &fence);
 
     while (!glfwWindowShouldClose(window)) {
+        int nwidth, nheight;
+        glfwGetFramebufferSize(window, &nwidth, &nheight);
+
+        if (nwidth != width || nheight != height) {
+            swapchain = std::make_unique<imr::Swapchain>(context, window);
+            width = nwidth;
+            height = nheight;
+            free(framebuffer);
+            framebuffer = reinterpret_cast<uint8_t*>(malloc(width * height * 4));
+
+            // unmap the old gpu buffer and reallocate it
+            vkUnmapMemory(context.device, buffer->memory);
+            buffer = std::make_unique<imr::Buffer>(context, width * height * 4, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+            CHECK_VK(vkMapMemory(context.device, buffer->memory, buffer->memory_offset, buffer->size, 0, (void**) &mapped_buffer), abort());
+        }
+
         vkWaitForFences(context.device, 1, &fence, VK_TRUE, UINT64_MAX);
         CHECK_VK(vkResetFences(context.device, 1, &fence), abort());
 
@@ -37,7 +52,7 @@ int main() {
             }
         }
         memcpy(mapped_buffer, framebuffer, width * height * 4);
-        swapchain.presentFromBuffer(buffer.handle, fence, std::nullopt);
+        swapchain->presentFromBuffer(buffer->handle, fence, std::nullopt);
 
         fps_counter.tick();
         fps_counter.updateGlfwWindowTitle(window);
