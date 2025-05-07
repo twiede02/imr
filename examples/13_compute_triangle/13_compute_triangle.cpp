e80ab69f92132c81c4ac6709893876ad68f21b5c
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-05-07 20:26:27 +0200

    use ComputeShader in other samples

--- examples/13_compute_triangle/13_compute_triangle.cpp
+++ examples/13_compute_triangle/13_compute_triangle.cpp
@@ -32,56 +32,7 @@ int main() {
 
     imr::FpsCounter fps_counter;
 
-    size_t spirv_bytes_count;
-    uint32_t* spirv_bytes;
-    if (!imr_read_file((std::filesystem::path(imr_get_executable_location()).parent_path().string() + "/13_compute_triangle.spv").c_str(), &spirv_bytes_count, (unsigned char**) &spirv_bytes))
-        abort();
-
-    VkShaderModule module;
-    CHECK_VK(vk.createShaderModule(tmp((VkShaderModuleCreateInfo) {
-        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-        .flags = 0,
-        .codeSize = spirv_bytes_count,
-        .pCode = spirv_bytes,
-    }), nullptr, &module), abort());
-
-    VkDescriptorSetLayout set0_layout;
-    CHECK_VK(vkCreateDescriptorSetLayout(vk.device, tmp((VkDescriptorSetLayoutCreateInfo) {
-        .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
-        .bindingCount = 1,
-        .pBindings = tmp((VkDescriptorSetLayoutBinding) {
-            .binding = 0,
-            .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
-            .descriptorCount = 1,
-            .stageFlags = VK_SHADER_STAGE_ALL,
-        }),
-    }), nullptr, &set0_layout), abort());
-
-    VkPipelineLayout layout;
-    CHECK_VK(vkCreatePipelineLayout(device.device, tmp((VkPipelineLayoutCreateInfo) {
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
-        .setLayoutCount = 1,
-        .pSetLayouts = &set0_layout,
-        .pushConstantRangeCount = 1,
-        .pPushConstantRanges = tmp((VkPushConstantRange) {
-            .stageFlags = VK_SHADER_STAGE_ALL,
-            .size = sizeof(push_constants),
-        })
-    }), nullptr, &layout), abort());
-
-    VkPipeline pipeline;
-    CHECK_VK(vkCreateComputePipelines(device.device, VK_NULL_HANDLE, 1, tmp((VkComputePipelineCreateInfo) {
-        .sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
-        .flags = 0,
-        .stage = {
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-            .flags = 0,
-            .stage = VK_SHADER_STAGE_COMPUTE_BIT,
-            .module = module,
-            .pName = "main",
-        },
-        .layout = layout,
-    }), nullptr, &pipeline), abort());
+    imr::ComputeShader shader(device, "13_compute_triangle.spv");
 
     VkDescriptorPool pool;
     vkCreateDescriptorPool(vk.device, tmp((VkDescriptorPoolCreateInfo) {
@@ -121,7 +72,7 @@ int main() {
                 .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
                 .descriptorPool = pool,
                 .descriptorSetCount = 1,
-                .pSetLayouts = &set0_layout,
+                .pSetLayouts = tmp(shader.set_layout(0)),
             }), &set);
 
             vkUpdateDescriptorSets(device.device, 1, tmp((VkWriteDescriptorSet) {
@@ -201,15 +152,15 @@ int main() {
 
             push_constants.time = ((imr_get_time_nano() / 1000) % 10000000000) / 1000000.0f;
 
-            vk.cmdBindPipeline(cmdbuf, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline);
-            vk.cmdBindDescriptorSets(cmdbuf, VK_PIPELINE_BIND_POINT_COMPUTE, layout, 0, 1, &set, 0, nullptr);
+            vk.cmdBindPipeline(cmdbuf, VK_PIPELINE_BIND_POINT_COMPUTE, shader.pipeline());
+            vk.cmdBindDescriptorSets(cmdbuf, VK_PIPELINE_BIND_POINT_COMPUTE, shader.layout(), 0, 1, &set, 0, nullptr);
 
             push_constants.tri = {
                 { -0.5, 0.5 },
                 { 0.0, -0.5 },
                 { 0.5, 0.5 }
             };
-            vk.cmdPushConstants(cmdbuf, layout, VK_SHADER_STAGE_ALL, 0, sizeof(push_constants), &push_constants);
+            vk.cmdPushConstants(cmdbuf, shader.layout(), VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(push_constants), &push_constants);
             vk.cmdDispatch(cmdbuf, (frame.width + 31) / 32, (frame.height + 31) / 32, 1);
 
             vk.cmdPipelineBarrier2KHR(cmdbuf, tmp((VkDependencyInfo) {
@@ -259,11 +210,7 @@ int main() {
 
     swapchain.drain();
 
-    vkDestroyPipeline(device.device, pipeline, nullptr);
-    vkDestroyShaderModule(device.device, module, nullptr);
     vkDestroyDescriptorPool(device.device, pool, nullptr);
-    vkDestroyPipelineLayout(device.device, layout, nullptr);
-    vkDestroyDescriptorSetLayout(device.device, set0_layout, nullptr);
 
     return 0;
 }
