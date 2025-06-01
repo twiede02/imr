8146a17467c47258bb8abfe2b26b9c5ccb007608
Hugo Devillers <hugo@xol.io>
2025-06-01 16:50:23 +0200

    dp

--- examples/15_compute_cubes/15_compute_cubes.glsl
+++ examples/15_compute_cubes/15_compute_cubes.glsl
@@ -25,24 +25,34 @@ float barCoord(vec2 a, vec2 b, vec2 point){
     return cross_2(PA, BA);
 }
 
-vec3 barycentricTri2(vec2 v0, vec2 v1, vec2 v2, vec2 point) {
-    float scaling = barCoord(v0.xy, v1.xy, v2.xy);
+double cross_2(dvec2 a, dvec2 b) {
+    return cross(dvec3(a, 0), dvec3(b, 0)).z;
+}
 
-    float u = barCoord(v0.xy, v1.xy, point) / scaling;
-    float v = barCoord(v1.xy, v2.xy, point) / scaling;
+double barCoord(dvec2 a, dvec2 b, dvec2 point){
+    dvec2 PA = point - a;
+    dvec2 BA = b - a;
+    return cross_2(PA, BA);
+}
+
+dvec3 barycentricTri2(dvec2 v0, dvec2 v1, dvec2 v2, dvec2 point) {
+    double scaling = barCoord(v0.xy, v1.xy, v2.xy);
+
+    double u = barCoord(v0.xy, v1.xy, point) / scaling;
+    double v = barCoord(v1.xy, v2.xy, point) / scaling;
 
     //if (scaling > 0)
     //    return vec3(-1);
 
-    return vec3(u, v, scaling);
+    return dvec3(u, v, scaling);
 }
 
-bool is_inside_edge(vec2 e0, vec2 e1, vec2 p) {
+bool is_inside_edge(dvec2 e0, dvec2 e1, dvec2 p) {
     if (e1.x == e0.x)
     return (e1.x > p.x) ^^ (e0.y > e1.y);
-    float a = (e1.y - e0.y) / (e1.x - e0.x);
-    float b = e0.y + (0 - e0.x) * a;
-    float ey = a * p.x + b;
+    double a = (e1.y - e0.y) / (e1.x - e0.x);
+    double b = e0.y + (0 - e0.x) * a;
+    double ey = a * p.x + b;
     return (ey < p.y) ^^ (e0.x > e1.x);
 }
 
@@ -51,8 +61,8 @@ void main() {
     if (gl_GlobalInvocationID.x >= img_size.x || gl_GlobalInvocationID.y >= img_size.y)
         return;
 
-    vec2 point = vec2(gl_GlobalInvocationID.xy) / vec2(img_size);
-    point = point * 2.0 - vec2(1.0);
+    dvec2 point = dvec2(gl_GlobalInvocationID.xy) / vec2(img_size);
+    point = point * 2.0 - dvec2(1.0);
 
     vec4 os_v0 = vec4(push_constants.triangle.v0, 1);
     vec4 os_v1 = vec4(push_constants.triangle.v1, 1);
@@ -60,14 +70,14 @@ void main() {
     vec4 v0 = push_constants.m * os_v0;
     vec4 v1 = push_constants.m * os_v1;
     vec4 v2 = push_constants.m * os_v2;
-    vec2 ss_v0 = v0.xy /= v0.w;
-    vec2 ss_v1 = v1.xy /= v1.w;
-    vec2 ss_v2 = v2.xy /= v2.w;
+    dvec2 ss_v0 = dvec2(v0.xy) / v0.w;
+    dvec2 ss_v1 = dvec2(v1.xy) / v1.w;
+    dvec2 ss_v2 = dvec2(v2.xy) / v2.w;
 
-    vec3 baryResults = barycentricTri2(ss_v0.xy, ss_v1.xy, ss_v2.xy, point);
-    float u = baryResults.x;
-    float v = baryResults.y;
-    float w = 1 - u - v;
+    dvec3 baryResults = barycentricTri2(ss_v0.xy, ss_v1.xy, ss_v2.xy, point);
+    double u = baryResults.x;
+    double v = baryResults.y;
+    double w = 1 - u - v;
 
     //if (!(u >= 0.0 && v >= 0.0 && w >= 0.0))
     //    return;
@@ -82,17 +92,17 @@ void main() {
     if (!frontface && !backface)
         return;
 
-    vec3 v_ws = vec3(v0.w, v1.w, v2.w);
-    vec3 ss_v_coefs = vec3(v, w, u);
-    vec3 pc_v_coefs = ss_v_coefs / v_ws;
-    float pc_v_coefs_sum = pc_v_coefs.x + pc_v_coefs.y + pc_v_coefs.z;
+    dvec3 v_ws = vec3(v0.w, v1.w, v2.w);
+    dvec3 ss_v_coefs = vec3(v, w, u);
+    dvec3 pc_v_coefs = ss_v_coefs / v_ws;
+    double pc_v_coefs_sum = pc_v_coefs.x + pc_v_coefs.y + pc_v_coefs.z;
     pc_v_coefs = pc_v_coefs / pc_v_coefs_sum;
 
-    float depth = dot(pc_v_coefs, vec3(v0.w, v1.w, v2.w));
+    float depth = float(dot(pc_v_coefs, vec3(v0.w, v1.w, v2.w)));
 
-    float tcx = dot(vec3(os_v0.x, os_v1.x, os_v2.x), pc_v_coefs);
-    float tcy = dot(vec3(os_v0.y, os_v1.y, os_v2.y), pc_v_coefs);
-    float tcz = dot(vec3(os_v0.z, os_v1.z, os_v2.z), pc_v_coefs);
+    float tcx = float(dot(vec3(os_v0.x, os_v1.x, os_v2.x), pc_v_coefs));
+    float tcy = float(dot(vec3(os_v0.y, os_v1.y, os_v2.y), pc_v_coefs));
+    float tcz = float(dot(vec3(os_v0.z, os_v1.z, os_v2.z), pc_v_coefs));
     vec3 tc = vec3(tcx, tcy, tcz);
     ivec3 tci = ivec3(tc * 16 + 0.001);
     c.xyz = vec3(1.0, 1.0, tc.x);
